19e2626b571b43b00d96ddc44493ddf477834a5c
Martin_VZ <martinvz2077@gmail.com>
2021-10-11 17:18:10 -0500

    Update settings.py
    Borrar secreto :)

--- authProject/settings.py
+++ authProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q%5b683jmg)sv+(x_68b7@d=!f&*^e8@yr6awr-!f)2u@$0zs5'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
